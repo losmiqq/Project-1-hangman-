b38160de5c2d57f6c016950b94e0571025dbbade
losmiqq <jevgengogo@gmail.com>
2026-09-18 19:14:50 +0200

    Split tasks into separate functions

--- project1.py
+++ project1.py
@@ -1,5 +1,6 @@
 import random as r
 
+# Запрос юзера продолжить/закончить игру
 def choose():
     while True:
         choose = input("Начать новую игру/Выйти (Start/Exit): ")
@@ -11,22 +12,30 @@ def choose():
             print("Вы ввели неправильный ответ")
 
 
-word_list = word_list = [
-    "велосипед", "компьютер", "алгоритм", "интернет", "клавиатура", 
-    "телефон", "самолёт", "автомобиль", "поезд", "корабль", "рюкзак", 
-    "холодильник", "телевизор", "пылесос", "зеркало", "кровать", "диван", 
-    "собака", "кошка", "медведь", "слон", "жираф", "крокодил", "пингвин", 
-    "дельфин", "акула", "черепаха", "яблоко", "апельсин", "банан", "арбуз", 
-    "картофель", "морковь", "помидор", "огурец", "шоколад", "бутерброд", 
-    "дерево", "цветок", "радуга", "облако", "гора", "водопад", "океан", 
-    "остров", "космос", "планета"
-    ]
+# word_list  = [
+#     "велосипед", "компьютер", "алгоритм", "интернет", "клавиатура", 
+#     "телефон", "самолёт", "автомобиль", "поезд", "корабль", "рюкзак", 
+#     "холодильник", "телевизор", "пылесос", "зеркало", "кровать", "диван", 
+#     "собака", "кошка", "медведь", "слон", "жираф", "крокодил", "пингвин", 
+#     "дельфин", "акула", "черепаха", "яблоко", "апельсин", "банан", "арбуз", 
+#     "картофель", "морковь", "помидор", "огурец", "шоколад", "бутерброд", 
+#     "дерево", "цветок", "радуга", "облако", "гора", "водопад", "океан", 
+#     "остров", "космос", "планета"
+#     ]
 
-def get_word(word_list):
-    return r.choice(word_list).upper() 
+# # Выбор случайного слова для игры
+# def get_word(word_list):
+#     return r.choice(word_list).upper() 
 
 
-# функция получения текущего состояния
+# Выбор случайного слова с файла
+def get_word():
+    with open ("C:\\Users\\jevge\\OneDrive\\Desktop\\projects\\gamewords.txt", "r", encoding="utf-8") as f:
+        word = r.choice(f.read().split()).upper()
+        return word
+
+
+# Функция получения текущего состояния
 def display_hangman(tries):
     stages = [  # финальное состояние: голова, торс, обе руки, обе ноги
                 '''
@@ -102,13 +111,29 @@ def display_hangman(tries):
     return stages[tries]
 
 
+# Состояние загаданного слова
+def word_update(word, guessed_letters):
+    result = "".join([i if i in guessed_letters else '_' for i in word])
+    return result
 
 
+# Проверка на валидность буквы
+def is_valid_letter(guessed_letters):
+    while True:
+        user_input = input("Введите букву: ").upper()
+
+        if user_input not in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ":
+            print("Вы ввели не букву!")
+        elif user_input in guessed_letters:
+            print("Вы уже называли эту букву!")
+        else:
+            return user_input
+
+
+# Функционал игры
 def play(word):
-    word_completion = '_' * len(word)
     guessed = False
     guessed_letters = []
-    guessed_words = []
     tries = 6
 
     print("Добро пожаловать в игру 'Виселица'!")
@@ -116,40 +141,28 @@ def play(word):
 
     while not guessed and tries > 0:
         print(display_hangman(tries))
-        print(word_completion)
-        user_input = input("Введите букву или слово целиком: ").upper()
-        for i in user_input:
-            if not i.isalpha():
-                print("Вы ввели не букву!")
-                user_input = input("Введите букву или слово целиком: ").upper()
+        print(word_update(word, guessed_letters))
+        print(f"Попыток осталось: {tries}")
+        user_input = is_valid_letter(guessed_letters)
+
+        if user_input in word:
+            guessed_letters.append(user_input)
+            print("Верно! Буква", user_input, "есть в слове.")
+            if word_update(word, guessed_letters).count("_") == 0:
+                guessed = True
+                print("Поздравляем, вы угадали слово! Вы победили!")
                 break
 
-        for i in user_input:
-            if i in guessed_letters:
-                print("Вы уже называли букву", i)
-                user_input = input("Введите букву или слово целиком: ").upper()
+        else:
+            tries -= 1
+            print("Буквы", user_input, "нет в слове.")
+            if tries == 0:
+                print("Вы проиграли! Загаданное слово было:", word)
                 break
-
-            elif i in word:
-                guessed_letters.append(i)
-                print("Верно! Буква", i, "есть в слове.")
-                word_completion = ''.join([i if i in guessed_letters else '_' for i in word])
-                print(word_completion)
-                if '_' not in word_completion:
-                    guessed = True
-                    print("Поздравляем, вы угадали слово! Вы победили!")
-                    break
-
-            else:
-                tries -= 1
-                print("Буквы", i, "нет в слове.")
-                if tries == 0:
-                    print("Вы проиграли! Загаданное слово было:", word)
-                    break
 
 while True:
     if choose():
-        play(get_word(word_list))
+        play(get_word())
     else:
         print("До свидания!")
         break
