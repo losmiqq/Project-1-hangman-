25ec3fc8f976203056d88bd95ba6fab8cc840a53
losmiqq <jevgengogo@gmail.com>
2026-09-22 16:31:51 +0200

    Add annotation to function

--- word_manager.py
+++ word_manager.py
@@ -1,13 +1,13 @@
 import random as r
 from pathlib import Path
-from constants import rus_letters
+from constants import alphabet_list
 
 
 BASE_DIR = Path(__file__).resolve().parent
 file_path = BASE_DIR / "gamewords.txt"
 
 
-def get_word():
+def get_word() -> None | str:
     try:
         with open (file_path, encoding="utf-8") as f:
             words = f.read().split()
@@ -19,7 +19,7 @@ def get_word():
 
             for word in words:
                 word = word.upper()
-                if len(word) > 4 and all(letter in rus_letters for letter in word):
+                if len(word) > 4 and all(letter in alphabet_list for letter in word):
                     valid_words.append(word)
 
             if not valid_words:
